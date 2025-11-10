aacad8ad06717e26a7f2040c99b97a0232a6eeb0
Deluxwe <nicomartinezbetancourt@gmail.com>
2025-11-09 22:26:39 -0500

    se añade otra vez

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerUser1, loginUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/usuario", registerUser1);
-
-export default router;
